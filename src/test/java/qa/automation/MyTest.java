3e8dcee867f8c95aaec94d26e7015863292c1ef7
micata88 <micata88@yahoo.com>
2023-08-22 14:30:06 +0300

    new exam

--- src/test/java/qa/automation/MyTest.java
+++ src/test/java/qa/automation/MyTest.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -26,6 +27,20 @@ public class MyTest {
         driver.get("https://www.saucedemo.com/");
 
         WebElement username = driver.findElement(By.id("user-name"));
+        username.click();
+        username.sendKeys("standard_user");
+
+        WebElement password = driver.findElement(By.xpath("(//input[@class='input_error form_input'])[2]"));
+        password.click();
+        password.sendKeys("secret_sauce");
+
+        WebElement loginButton = driver.findElement(By.cssSelector("[value=Login]"));
+        loginButton.click();
+
+        WebElement userMenuButton = driver.findElement(By.id("react-burger-menu-btn"));
+        Assert.assertTrue(userMenuButton.isDisplayed(),"This shall be visible after successfull Login");
+
+
 
     }
 }
