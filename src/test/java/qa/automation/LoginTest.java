c581c0b4319aedac5024989c40b7fd1c177efc00
micata88 <micata88@yahoo.com>
2023-08-22 16:36:34 +0300

    new exam

--- src/test/java/qa/automation/LoginTest.java
+++ src/test/java/qa/automation/LoginTest.java
@@ -1,5 +1,6 @@
 package qa.automation;
 
+import com.opencsv.exceptions.CsvException;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -10,6 +11,9 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+import utils.CsvHelper;
+
+import java.io.IOException;
 
 public class LoginTest {
     private WebDriver driver;
@@ -29,8 +33,14 @@ public class LoginTest {
         return new  Object[][]{
                 {"adriana22","secret_sauce"},
                 {"standard_user","petya84"},
-                {"blah","blah"}
+                {"blah","blah"},
+                {"","secret_sauce"},
         };
+    }
+
+    @DataProvider(name = "csvUserList")
+    public static Object[][] readUsersFromCsvFile() throws IOException, CsvException {
+        return CsvHelper.readCsvFile("src/test/resources/users.csv");
     }
 
     @Test(dataProvider = "wrongUserName" )
@@ -51,7 +61,23 @@ public class LoginTest {
         WebElement errorLoginText = driver.findElement(By.xpath("//*[text()='Epic sadface: Username and password do not match any user in this service']"));
 
         Assert.assertTrue(errorLoginText.isDisplayed());
+    }
+    @Test(dataProvider = "csvUserList")
+    public void SuccessfullLogin(String userName,String password){
+        driver.get("https://www.saucedemo.com/");
 
+        WebElement username = driver.findElement(By.id("user-name"));
+        username.click();
+        username.sendKeys(userName);
 
+        WebElement passwordInput = driver.findElement(By.xpath("(//input[@class='input_error form_input'])[2]"));
+        passwordInput.click();
+        passwordInput.sendKeys(password);
+
+        WebElement loginButton = driver.findElement(By.cssSelector("[value=Login]"));
+        loginButton.click();
+
+        WebElement userMenuButton = driver.findElement(By.id("react-burger-menu-btn"));
+        Assert.assertTrue(userMenuButton.isDisplayed(),"This shall be visible after successfully Login");
     }
 }
