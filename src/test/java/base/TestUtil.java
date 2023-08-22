8d71a56bcc5a0b29c3174e595d573bd878e557bc
micata88 <micata88@yahoo.com>
2023-08-22 17:32:55 +0300

    new exam

--- src/test/java/base/TestUtil.java
+++ src/test/java/base/TestUtil.java
@@ -8,9 +8,15 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class TestUtil {
     public WebDriver driver;
-    private String url = "https://www.saucedemo.com/";
+    private String url; // = "https://www.saucedemo.com/";
+    private String browser;
+    private  int implicitWait;
 
     @BeforeMethod
     public void SetUp(){
@@ -22,6 +28,15 @@ public class TestUtil {
         driver.quit();
     }
     private void setupBrowserDriver(){
+        try (FileInputStream configFile = new FileInputStream("src/test/resources/users.config.properties ")){
+            Properties config = new Properties();
+            config.load(configFile);
+            url = config.getProperty("urlAddress");
+            browser = config.getProperty("browser");
+            implicitWait = Integer.parseInt(config.getProperty("implicitWait"));
+        }catch (IOException e){
+            System.out.println("Cannot read configs");
+        }
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
     }
